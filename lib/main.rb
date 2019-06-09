56a1578b68bfd206979b22b25afaf0164a16d4b1
Esther <esther.bsb@gmail.com>
2019-06-09 09:37:51 -0300

    Envia 1 BCY para endereço especificado na classe
    usando o SKD da BlockCypher

--- lib/main.rb
+++ lib/main.rb
@@ -6,6 +6,18 @@ class Main
     @address = "COLOQUE O ENDERECO AQUI"
   end
 
-  def fundWallet()
+  # Transfere BCY para o endereço @address
+  #
+  # Params:
+  # amount: quantidade a ser enviada em BCY
+  #
+  def transfer(amount)
+    puts "\n\nEnviando " + amount.to_s + " BCY para " + @address
+    block_cypher = BlockCypher::Api.new(currency:BlockCypher::BCY, network:BlockCypher::TEST_NET, api_token: @token)
+    block_cypher.faucet(@address, amount)
+    puts "Concluído"
   end
 end
+
+main = Main.new
+main.transfer(1)
