272154e21da7605ac34f8b2f84263046c0b5ff58
Esther <esther.bsb@gmail.com>
2019-06-09 10:11:05 -0300

    Adiciona testes

--- test/main_test.rb
+++ test/main_test.rb
@@ -5,4 +5,21 @@ class TestMain < Minitest::Test
   def setup
     @main = Main.new
   end
+
+  def test_envia_1BCY
+    assert_equal true, @main.transfer(1)
+  end
+
+  def test_envia_valor_negativo
+    assert_equal false, @main.transfer(-1)
+  end
+
+  def test_envia_valor_decimal
+    assert_equal false, @main.transfer(0.1)
+  end
+
+  def test_envia_zero
+    assert_equal false, @main.transfer(0)
+  end
+
 end
